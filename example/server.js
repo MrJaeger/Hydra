38892ea217f7647e160148f187ed2825c1a4d2d6
Andrew Jaeger <mrjaeger00@gmail.com>
2014-09-14 22:43:08 -0700

    get dem semi colons out of there

--- example/server.js
+++ example/server.js
@@ -1,10 +1,11 @@
-var nodeStatic = require('node-static');
-var http = require('http');
-var file = new(nodeStatic.Server)();
-var app = http.createServer(function (req, res) {
-  file.serve(req, res);
-}).listen(3000);
+var nodeStatic = require('node-static')
+    , http = require('http')
+    , server = new(nodeStatic.Server)()
 
-var io = require('socket.io').listen(app);
+var app = http.createServer(function (req, res) {
+    server.serve(req, res)
+}).listen(3000)
+
+var io = require('socket.io').listen(app)
 var hydra = require('../dist/hydra-server.js')
 hydra.start(io)
